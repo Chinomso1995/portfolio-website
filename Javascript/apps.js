0f93b220f8dcc949de8881dc1bb978c853fa5fe5
Chinomso Johnson <61166223+Chinomso1995@users.noreply.github.com>
2020-08-04 23:17:57 +0100

    Add files via upload

--- Javascript/apps.js
+++ Javascript/apps.js
@@ -11,24 +11,17 @@ tl.to('.resume', {opacity: '1', visibility: 'visible', yPercent: -5, duration: .
 const navIcon = document.querySelector('.navbar');
 navIcon.addEventListener('click', ()=>{
   if (tl.reversed()) {
+    navIcon.classList.add('nav-anim');
     tl.play()
     document.body.classList.add('noScroll');
 } else {
+    navIcon.classList.remove('nav-anim');
     tl.reverse()
     document.body.classList.remove('noScroll');
 }
 })
 //remove resume icon
-const resumeNavIcon = document.querySelector('.resume-navbar');
-resumeNavIcon.addEventListener('click', ()=>{
-  if (tl.reversed()) {
-    tl.play()
-    document.body.classList.add('noScroll');
-} else {
-    tl.reverse()
-    document.body.classList.remove('noScroll');
-}
-})
+
 const allLinks = document.querySelectorAll('.list-items li a');
 allLinks.forEach(link => {
   link.addEventListener('click', () => {
